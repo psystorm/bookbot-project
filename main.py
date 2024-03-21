fb899087c8fa0b7a325ec8e446cd5e3d75df4e44
psystorm <novapsystorm@gmail.com>
2024-03-21 08:34:33 -0600

    Read text, static analysis

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print('hello world')
+
+def word_count(strg):
+    return len(strg.split())
+
+def letter_count(strg):
+    lc = {}
+    words = strg.lower().split()
+    for word in words:
+        for letter in word:
+            if letter in lc:
+                lc[letter] += 1
+            else:
+                lc[letter] = 1
+    return lc
+
+def main():
+    file_contents = ''
+    with open("books/Frankenstein.txt") as f:
+        file_contents = f.read()
+    print(word_count(file_contents))
+    print(letter_count(file_contents))
+
+main()
