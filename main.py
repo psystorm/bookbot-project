fa1cb1e9afb3d1868abf03f5f3a92ffd48e9dc52
psystorm <novapsystorm@gmail.com>
2024-03-21 11:17:45 -0600

    Added Report function

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ def word_count(strg):
     return len(strg.split())
 
 def letter_count(strg):
-    return collections.Counter(c for word in strg.lower().split() for c in word)
+    return collections.Counter(c for word in strg.lower().split() for c in word if c.isalpha())
+
+def book_report(f_name,strg):
+    print(f'--- Begin Report of {f_name} ---')
+    print(f'Total Word Count: {word_count(strg)}')
+    for elem in letter_count(strg).most_common():
+        print(f"The '{elem[0]}' character was found {elem[1]} times")
+    print(f'--- End Report of {f_name} ---')
 
 def main():
     file_contents = ''
     with open("books/Frankenstein.txt") as f:
         file_contents = f.read()
-    print(word_count(file_contents))
-    print(letter_count(file_contents))
+    
+    book_report(f.name,file_contents)
 
 main()
